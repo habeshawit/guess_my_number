9b0f548adcff60a9f5a3d290aaa9697779d4328d
habeshawit <shalomseleshi@gmail.com>
2021-08-26 12:00:19 -0700

    implement game logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random()*20) + 1      
+let score = 20
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value)
+
+    if(!guess){
+        document.querySelector('.message').textContent = "⛔ No number!"
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = "🎉 Correct Number!"
+    } else if(guess > secretNumber){
+        document.querySelector('.message').textContent = "📈 Too high!"
+        score--
+        document.querySelector('.score').textContent = score
+    } else if(guess < secretNumber){
+        document.querySelector('.message').textContent = "📉 Too low!"
+        score--
+        document.querySelector('.score').textContent = score
+    }
+})
