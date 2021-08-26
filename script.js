3e038425a1da70b15be0dfae39de856f9d341a2a
habeshawit <shalomseleshi@gmail.com>
2021-08-26 12:14:25 -0700

    refactor logic into function

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const secretNumber = Math.trunc(Math.random()*20) + 1
 let score = 20
 console.log(secretNumber);
 
+function updateScore(guess){
+    console.log(score);
+    if(score === 1){
+        document.querySelector('.message').textContent = "💥 You lost the game!"
+        score = 0
+        document.querySelector('.score').textContent = score
+        return
+    }
+    if(guess > secretNumber){
+        document.querySelector('.message').textContent = "📈 Too high!"
+    } else {
+        document.querySelector('.message').textContent = "📉 Too low!"
+    }
+    score--
+    document.querySelector('.score').textContent = score
+}
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value)
 
@@ -11,13 +28,7 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = "⛔ No number!"
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!"
-    } else if(guess > secretNumber){
-        document.querySelector('.message').textContent = "📈 Too high!"
-        score--
-        document.querySelector('.score').textContent = score
-    } else if(guess < secretNumber){
-        document.querySelector('.message').textContent = "📉 Too low!"
-        score--
-        document.querySelector('.score').textContent = score
+    } else {
+        updateScore(guess)
     }
 })
